52582f61d6fc29a1b6b287a43a08dfab37ee4595
harshalofic <mr.hv98@gmail.com>
2025-03-03 12:23:41 +0530

    Delete _next/static directory

--- _next/static/y1j8CjYaV4vjZaj_Be4NF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-350d9d86556f6d87.js"],"/[[...slug]]":["static/chunks/pages/[[...slug]]-9329d20825c26f37.js"],sortedPages:["/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
